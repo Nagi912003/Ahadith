7dcda148cbf9d415d2451dde8def25ed7c9dd15b
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2023-07-04 14:01:26 +0300

    V2.2.5.6 added singleton pattern for the theme manager class

--- ahadith/lib/theme/theme_manager.dart
+++ ahadith/lib/theme/theme_manager.dart
@@ -2,84 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 class ThemeManager with ChangeNotifier {
-    final Box _savedBox = Hive.box('saved');
-    ThemeManager(){
-      if(!_savedBox.containsKey('themeMode')){
+  final Box _savedBox = Hive.box('saved');
+
+  ThemeManager._private() {
+    // Private constructor
+    if (!_savedBox.containsKey('themeMode')) {
+      _savedBox.put('themeMode', 1);
+    } else {
+      toggleBackGroundImage(_savedBox.get('themeMode'));
+    }
+  }
+
+  static final ThemeManager _instance = ThemeManager._private();
+
+  factory ThemeManager() {
+    return _instance;
+  }
+
+  String bgImage = 'assets/images/watercolor.png';
+  int mode = 1;
+  Color appPrimaryColor = Colors.deepPurple;
+  Color appPrimaryColor200 = Colors.deepPurple[200]!;
+  Color appPrimaryColorInverse = Colors.deepPurple[200]!;
+
+  void setBackGroundImage(String imagePath) {
+    bgImage = imagePath;
+    notifyListeners();
+  }
+
+  void toggleBackGroundImage(int mode) {
+    switch (mode) {
+      case 1:
         _savedBox.put('themeMode', 1);
-      }else{
-        toggleBackGroundImage(_savedBox.get('themeMode'));
-      }
+        appPrimaryColor = Colors.deepPurple[200]!;
+        appPrimaryColor200 = Colors.deepPurple[100]!;
+        appPrimaryColorInverse = Colors.deepPurple[200]!;
+        bgImage = 'assets/images/watercolor.png';
+        break;
+      case 2:
+        _savedBox.put('themeMode', 2);
+        appPrimaryColor = Colors.deepPurple;
+        appPrimaryColor200 = Colors.deepPurple[200]!;
+        appPrimaryColorInverse = Colors.deepPurple;
+        bgImage = 'assets/images/watercolor-p.png';
+        break;
+      case 3:
+        _savedBox.put('themeMode', 3);
+        appPrimaryColor = Colors.indigo[200]!;
+        appPrimaryColor200 = Colors.indigo[100]!;
+        appPrimaryColorInverse = Colors.indigo;
+        bgImage = 'assets/images/watercolor-off.png';
+        break;
+      case 4:
+        _savedBox.put('themeMode', 4);
+        appPrimaryColor = Colors.blueGrey;
+        appPrimaryColor200 = Colors.blueGrey[200]!;
+        appPrimaryColorInverse = Colors.blueGrey;
+        bgImage = 'assets/images/watercolor-b.png';
+        break;
+      case 5:
+        _savedBox.put('themeMode', 5);
+        appPrimaryColor = Colors.green[200]!;
+        appPrimaryColor200 = Colors.green[100]!;
+        appPrimaryColorInverse = Colors.red;
+        bgImage = 'assets/images/watercolor-g.png';
+        break;
+      case 7:
+        _savedBox.put('themeMode', 7);
+        appPrimaryColor = Colors.blue;
+        appPrimaryColor200 = Colors.blue[300]!;
+        appPrimaryColorInverse = Colors.red;
+        bgImage = 'assets/images/watercolor-bw.png';
+        break;
+      case 8:
+        _savedBox.put('themeMode', 8);
+        appPrimaryColor = Colors.grey;
+        appPrimaryColor200 = Colors.grey;
+        appPrimaryColorInverse = Colors.grey;
+        bgImage = 'assets/images/watercolor-grey.png';
+        break;
+      default:
+        appPrimaryColor = Colors.deepPurple[200]!;
+        appPrimaryColor200 = Colors.deepPurple[100]!;
+        appPrimaryColorInverse = Colors.deepPurple[200]!;
+        bgImage = 'assets/images/watercolor.png';
     }
-
-    String bgImage = 'assets/images/watercolor.png';
-    int mode = 1;
-    Color appPrimaryColor = Colors.deepPurple;
-    Color appPrimaryColor200 = Colors.deepPurple[200]!;
-    Color appPrimaryColorInverse = Colors.deepPurple[200]!;
-
-    void setBackGroundImage(String imagePath) {
-      bgImage = imagePath;
-      notifyListeners();
-    }
-
-    void toggleBackGroundImage(int mode) {
-      switch (mode) {
-        case 1:
-          _savedBox.put('themeMode', 1);
-          appPrimaryColor = Colors.deepPurple[200]!;
-          appPrimaryColor200 = Colors.deepPurple[100]!;
-          appPrimaryColorInverse = Colors.deepPurple[200]!;
-          bgImage = 'assets/images/watercolor.png';
-          break;
-        case 2:
-          _savedBox.put('themeMode', 2);
-          appPrimaryColor = Colors.deepPurple;
-          appPrimaryColor200 = Colors.deepPurple[200]!;
-          appPrimaryColorInverse = Colors.deepPurple;
-          bgImage = 'assets/images/watercolor-p.png';
-          break;
-        case 3:
-          _savedBox.put('themeMode', 3);
-          appPrimaryColor = Colors.indigo[200]!;
-          appPrimaryColor200 = Colors.indigo[100]!;
-          appPrimaryColorInverse = Colors.indigo;
-          bgImage = 'assets/images/watercolor-off.png';
-          break;
-        case 4:
-          _savedBox.put('themeMode', 4);
-          appPrimaryColor = Colors.blueGrey;
-          appPrimaryColor200 = Colors.blueGrey[200]!;
-          appPrimaryColorInverse = Colors.blueGrey;
-          bgImage = 'assets/images/watercolor-b.png';
-          break;
-        case 5:
-          _savedBox.put('themeMode', 5);
-          appPrimaryColor = Colors.green[200]!;
-          appPrimaryColor200 = Colors.green[100]!;
-          appPrimaryColorInverse = Colors.red;
-          bgImage = 'assets/images/watercolor-g.png';
-          break;
-        case 7:
-          _savedBox.put('themeMode', 7);
-          appPrimaryColor = Colors.blue;
-          appPrimaryColor200 = Colors.blue[300]!;
-          appPrimaryColorInverse = Colors.red;
-          bgImage = 'assets/images/watercolor-bw.png';
-          break;
-        case 8:
-          _savedBox.put('themeMode', 8);
-          appPrimaryColor = Colors.grey;
-          appPrimaryColor200 = Colors.grey;
-          appPrimaryColorInverse = Colors.grey;
-          bgImage = 'assets/images/watercolor-grey.png';
-          break;
-        default:
-          appPrimaryColor = Colors.deepPurple[200]!;
-          appPrimaryColor200 = Colors.deepPurple[100]!;
-          appPrimaryColorInverse = Colors.deepPurple[200]!;
-          bgImage = 'assets/images/watercolor.png';
-      }
-      notifyListeners();
-    }
-
+    notifyListeners();
+  }
 }
